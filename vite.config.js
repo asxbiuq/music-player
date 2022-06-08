c8ffaea61eb873ab3281a9361f6dd9890b65d8cb
17o3 <17o3gutk29vinqb@gmail.com>
2022-06-08 17:09:50 +0800

    layout

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,14 @@ import path from 'path'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import Pages from 'vite-plugin-pages'
+import Layouts from 'vite-plugin-vue-layouts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue({ 
+    vue({
       //$语法糖
-      reactivityTransform: true 
+      reactivityTransform: true
     }),
     AutoImport({
       // targets to transform
@@ -79,38 +80,38 @@ export default defineConfig({
     }),
     Components({
       // relative paths to the directory to search for components.
-      dirs: ['src/components','src/views','src/pages'],
-    
+      dirs: ['src/components', 'src/views', 'src/pages'],
+
       // valid file extensions for components.
       extensions: ['vue'],
       // search for subdirectories
       deep: true,
       // resolvers for custom components
       resolvers: [],
-    
+
       // generate `components.d.ts` global declarations,
       // also accepts a path for custom filename
       // default: `true` if package typescript is installed
       dts: false,
-    
+
       // Allow subdirectories as namespace prefix for components.
       directoryAsNamespace: false,
       // Subdirectory paths for ignoring namespace prefixes
       // works when `directoryAsNamespace: true`
       globalNamespaces: [],
-    
+
       // auto import for directives
       // default: `true` for Vue 3, `false` for Vue 2
       // Babel is needed to do the transformation for Vue 2, it's disabled by default for performance concerns.
       // To install Babel, run: `npm install -D @babel/parser @babel/traverse`
       directives: true,
-    
+
       // Transform path before resolving
       importPathTransform: v => v,
-    
+
       // Allow for components to override other components with the same name
       allowOverrides: false,
-    
+
       // filters for transforming targets
       include: [/\.vue$/, /\.vue\?vue/],
       exclude: [/[\\/]node_modules[\\/]/, /[\\/]\.git[\\/]/, /[\\/]\.nuxt[\\/]/],
@@ -118,7 +119,10 @@ export default defineConfig({
     Pages({
       dirs: 'src/pages',  // File directory where routing needs to be generated
       exclude: ['**/components/*.vue']  // Excluded directories, i.e. the directories under all components directories are not included vue file generation route
-  }),
+    }),
+    Layouts({
+      defaultLayout: 'default'
+    }),
   ],
   resolve: {
     alias: {
