7288e0d4b7cfa1b066336a88da5b793fff7e5847
17o3 <17o3gutk29vinqb@gmail.com>
2022-06-08 17:29:00 +0800

    layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,8 +1,26 @@
 <template>
-  <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
-    <div class="w-1/4 m-auto text-center text-gray-300 bg-teal-800">
-      Default Layout
-    </div>
-    <router-view />
-  </main>
+
+  <div class="playlist h-full flex-col gap-2">
+
+
+    <RouterView v-slot="{ Component }">
+      <template v-if="Component">
+        <!-- <Transition mode="out-in"> -->
+          <!-- 防止页面数据不更新,排除 PlaylistDetails组件,这里与Page插件不兼容!!!!!!名字还是PlaylistDetails -->
+          <KeepAlive exclude="PlaylistDetails">
+            <Suspense>
+              <!-- 主要内容 -->
+              <component :is="Component"></component>
+
+              <!-- 加载中状态 -->
+              <template #fallback class="bg-black-500">
+                <SkeletonUserPlaylists/>
+              </template>
+            </Suspense>
+          </KeepAlive>
+        <!-- </Transition> -->
+      </template>
+    </RouterView>
+
+  </div>
 </template>
