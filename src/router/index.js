d4943071d1c9e1afbb352b1c3c8b9cbfc2e3dcff
17o3 <17o3gutk29vinqb@gmail.com>
2022-06-08 17:06:35 +0800

    router beforeEach

--- src/router/index.js
+++ src/router/index.js
@@ -2,90 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { auth } from '@/firebase/config'
 import routes from '~pages'
 
-
-const requireAuth = (to, from, next) => {
-  let user = auth.currentUser
-  if (!user) {
-    next({ name: 'Login' })
-  } else {
-    next()
-  }
-}
-
-// const routes = [
-//   {
-//     path: '/',
-//     name: 'Home',
-//     component: () => import('views/Home.vue'),
-//   },
-//   {
-//     path: '/auth',
-//     name: 'Auth',
-//     component: () => import('views/Auth.vue'),
-//     redirect: { name: 'Login' },
-//     children: [
-//       {
-//         path: '/login',
-//         name: 'Login',
-//         component: () => import('views/auth/Login.vue'),
-//       },
-//       {
-//         path: '/signup',
-//         name: 'Signup',
-//         component: () => import('views/auth/Signup.vue'),
-//       },
-//     ],
-//   },
-//   {
-//     path: '/playlists',
-//     name: 'PlayLists',
-//     component: () => import('views/PlayLists.vue'),
-//     beforeEnter: requireAuth,
-//     redirect: { name: 'UserPlaylists' },
-//     children: [ 
-//       //子路由create 如果携程/create ,则代表到根路径的create ,而不是/playlists/create
-//       {
-//         path: 'create',
-//         name: 'CreatePlaylist',
-//         component: () => import('views/playlists/CreatePlaylist.vue'),
-//         beforeEnter: requireAuth
-//       },
-//       {
-//         path: ':id',
-//         name: 'PlaylistDetails',
-//         component: () => import('views/playlists/PlaylistDetails.vue'),
-//         beforeEnter: requireAuth,
-//         props: true,
-//         redirect: { name: 'SongList' },
-//         children: [
-//           {
-//             path: 'addsong',
-//             name: 'AddSong',
-//             component: () => import('components/AddSong.vue'),
-//             beforeEnter: requireAuth
-//           },
-//           {
-//             path: 'songlist',
-//             name: 'SongList',
-//             component: () => import('components/SongList.vue'),
-//             beforeEnter: requireAuth
-//           },
-//         ]
-//       },
-//       {
-//         path: 'user',
-//         name: 'UserPlaylists',
-//         component: () => import('views/playlists/UserPlaylists.vue'),
-//         beforeEnter: requireAuth
-//       },
-//     ]
-//   },
-
-// ]
-
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+router.beforeEach((to, from, next) => {
+  if ((to.name !== 'Auth-Login' || to.name !== 'Auth-Signup') && !auth.currentUser) next({ name: 'Auth-Login' })
+  else next()
 })
 
 export default router
