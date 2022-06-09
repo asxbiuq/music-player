e3e4377b830a73f9a1af5d0949de48530620b36a
17o3 <17o3gutk29vinqb@gmail.com>
2022-06-09 23:19:59 +0800

    $语法糖改写

---
+++ src/composables/useUser.js
@@ -0,0 +1,20 @@
+import { ref } from 'vue'
+import { auth } from '@/firebase/config'
+// useUser在后台执行.所以没必要显示动画
+
+// refs
+const user = $ref(auth.currentUser)
+
+
+
+// 监听用户身份更改变换,只需一个监听器
+auth.onAuthStateChanged(_user => {
+  console.log('User state change. Current user is:', _user)
+  user = _user
+});
+
+const getUser = () => {
+  return $$({ user }) 
+}
+
+export default getUser
