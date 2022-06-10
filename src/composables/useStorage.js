4ab0bc3f1ee839666e13f7280d7fdc6a2570ecff
17o3 <17o3gutk29vinqb@gmail.com>
2022-06-10 22:27:41 +0800

    上传音乐

--- src/composables/useStorage.js
+++ src/composables/useStorage.js
@@ -20,7 +20,7 @@ const useStorage = () => {
         filePath = `covers/${user.uid}/${file.name}`
         const storageRef = fireRef(storage, filePath);
         try {
-            //需要先上传,所以这里需要阻塞以下
+            //需要先上传,所以这里需要阻塞一下
             await uploadBytes(storageRef, file)
                 .then((snapshot) => {
                     console.log('Uploaded file!')
@@ -56,21 +56,53 @@ const useStorage = () => {
             // Uh-oh, an error occurred!
             console.log(err.message)
             error = err.message
-        });
-        // try {
-        //     await deleteDoc(doc(db, col, doc));
-
-        //     error = null
-        //     isPending = false
-        // } catch (err) {
-        //     console.log(err.message)
-        //     error = err.message
-        // }
+        })
     }
 
+    const uploadMusic = async (file) => {
+        isPending = true
+        filePath = `musics/${user.uid}/${file.name}`
+        const storageRef = fireRef(storage, filePath);
+        try {
+            //需要先上传,所以这里需要阻塞一下
+            await uploadBytes(storageRef, file)
+                .then((snapshot) => {
+                    console.log('Uploaded file!')
+                })
+            const url = $(await getDownloadURL(storageRef)
+                .then(url => {
+                    console.log('Gated Uploaded file Url: ',url)
+                    return url
+                }))
+            error = null
+            isPending = false
+            // return url
+            return $$({ url })
+        } catch (err) {
+            console.log(err.message)
+            error = err
+        }
+        
+    }
+    const deleteMusic = async (filePath) => {
+        isPending = true
+        // filePath = `covers/${user.uid}/${file.name}`
+        const desertRef = fireRef(storage, filePath);
+        // Delete the file
+        deleteObject(desertRef).then(() => {
+            // File deleted successfully
 
+            console.log(filePath, " is delete success")
+            error = null
+            isPending = false
+        }).catch((err) => {
+            // Uh-oh, an error occurred!
+            console.log(err.message)
+            error = err.message
+        })
+    }
 
-    return $$({  filePath, error, uploadImage, deleteImage, isPending })
+    return $$({  filePath, error, isPending,uploadImage, deleteImage,  uploadMusic, deleteMusic })
 }
 
 export default useStorage
--- src/pages/Playlists/PlaylistDetails/AddSong.vue
+++ src/pages/Playlists/PlaylistDetails/AddSong.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="add-song ">
+    <div class="add-song">
         <!-- <button v-if="!showForm" @click="showForm = true">Add Songs</button> -->
         <form @submit.prevent="handleSubmit">
             <label>Add a New Song</label>
             <input type="text" placeholder="Song title" required v-model="title">
             <input type="text" placeholder="Artist" required v-model="artist">
+            <input type="file" @change="handleSelected">
+            <div class="error">{{ fileError }}</div>
             <div class="container text-center">
                 <button  v-if="!isPending">Add</button>
                 <button  v-else disabled>Adding...</button>
@@ -17,19 +19,27 @@
 </template>
 
 <script setup>
+const file = $ref(null)
+const fileError = $ref(null)
 const playlist = $(inject('playlist'))
+
 const emits = defineEmits(['addedSong'])
 
 console.log('playlist:', playlist)
 const title = $ref('')
 const artist = $ref('')
 // const showForm = $ref(false)
-const { updateDoc, isPending } = useDocument()
+const { uploadMusic, filePath, isPending : MusicPending } = $(useStorage())
+const { updateDoc, isPending : docPending } = useDocument()
 const router = useRouter()
+
 const handleSubmit = async () => {
+    const { url } = $(await uploadMusic(file))
     const newSong = {
         title: title,
         artist: artist,
+        musicUrl: url,
+        filePath: filePath,
         id: Math.floor(Math.random() * 100000000)
     }
     // 上传数据
@@ -39,11 +49,25 @@ const handleSubmit = async () => {
     // 更新本地的数据
     playlist.songs.push(newSong)
     // console.log(playlist)
-    // 清空表格里的数据
-    title = ''
-    artist = ''
+    // 这里不需要清空表格里的数据,因为若是上传成功,页面则跳转,数据则被重置,若失败则保留输入信息
+
     emits('addedSong')
     router.push({ name: 'Playlists-PlaylistDetails-SongList' })
+}
+// 允许上传的数据类型
+const types = ['audio/mpeg', 'audio/ogg']
+
+const handleSelected = (e) => {
+    const selected = e.target.files[0]
+    console.log(selected)
+
+    if (selected && types.includes(selected.type)) {
+        file = selected
+        fileError = null
+    } else {
+        file = null
+        fileError = 'Please select an image file (mp3 or ogg)'
+    }
 }
 </script>
 
